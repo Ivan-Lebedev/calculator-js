d11e3975651d48f5b7a0f6595eaaebcb50af5c27
ivan <ivieee@yandex.ru>
2020-09-22 22:13:50 +0400

    feat: sqrt, pow, negative nums, fractions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
+const sqrtButton = document.querySelector('[data-operation-sqrt]')
+const plusMinusButton = document.querySelector('[data-plus-minus]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
@@ -40,19 +42,29 @@ class Calculator {
         let computation
         const prev = parseFloat(this.previousOperand)
         const current = parseFloat(this.currentOperand)
+        if (!isNaN(prev) && this.operation === '√') {
+            computation = Math.sqrt(prev * 1e10)
+            this.currentOperand = computation / Math.sqrt(1e10)
+            this.operation = undefined
+            this.previousOperand = ''
+            // this.updateDisplay()
+        }
         if (isNaN(prev) || isNaN(current)) return
         switch (this.operation) {
             case '+':
-                computation = prev + current
+                computation = (prev * 1e10 + current * 1e10) / 1e10
                 break
             case '-':
-                computation = prev - current
+                computation = (prev * 1e10 - current * 1e10) / 1e10
                 break
             case '*':
-                computation = prev * current
+                computation = (prev * 1e10 * current * 1e10) / 1e10 / 1e10
                 break
             case '÷':
                 computation = prev / current
+                break
+            case '^':
+                computation = prev ** current
                 break
             default:
                 return
@@ -63,11 +75,15 @@ class Calculator {
     }
     getDisplayNumber(number) {
         const stringNumber = String(number)
+        if (isNaN(number)) {
+            return 'Error, press AC'
+        }
         const integerDigits = parseFloat(stringNumber.split('.')[0])
         const decimalDigits = stringNumber.split('.')[1]
         let integerDisplay
+
         if (isNaN(integerDigits)) {
-            integerDisplay = ''
+            integerDisplay = '0'
         } else {
             integerDisplay = integerDigits.toLocaleString('en', {
                 maximumFractionDigits: 0
@@ -80,14 +96,24 @@ class Calculator {
         }
     }
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
-        if (this.operation != null) {
-            this.previousOperandTextElement.innerText =
-                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        if (this.currentOperand === '.') {
+            this.currentOperandTextElement.innerText = '0.'
         } else {
-            this.previousOperandTextElement.innerText = ''
+            this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+            if (this.operation === '√') {
+                this.previousOperandTextElement.innerText =
+                    `${this.operation} (${this.getDisplayNumber(this.previousOperand)})`
+            } else if (this.operation != null) {
+                this.previousOperandTextElement.innerText =
+                    `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+            } else {
+                this.previousOperandTextElement.innerText = ''
+            }
         }
 
+    }
+    toggleMinus() {
+        this.currentOperand = -this.currentOperand
     }
 }
 
@@ -119,5 +145,15 @@ allClearButton.addEventListener('click', button => {
 
 deleteButton.addEventListener('click', button => {
     calculator.delete()
+    calculator.updateDisplay()
+})
+
+sqrtButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+plusMinusButton.addEventListener('click', button => {
+    calculator.toggleMinus()
     calculator.updateDisplay()
 })
